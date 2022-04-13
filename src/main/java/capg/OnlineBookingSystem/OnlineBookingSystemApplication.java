f0e751a5090f2d60f503ba131e89e35757c06739
unknown <ravit3981@gmail.com>
2022-04-13 17:15:07 +0530

    First Comment

---
+++ src/main/java/capg/OnlineBookingSystem/OnlineBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package capg.OnlineBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capg/OnlineBookingSystem/OnlineBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package capg.OnlineBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
